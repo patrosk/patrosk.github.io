4422fae364aa5857f6dd3c9dc922a6b79d50c673
patrosk <naiade99@gmail.com>
2021-09-27 13:40:41 +0200

    added html and js

---
+++ app.js
@@ -0,0 +1,25 @@
+// var board = document.getElementById("surf")
+
+// document.getElementById("surf").textContent = "Hey surfer!"
+
+// document.getElementById("surf").textContent += " I like your style"
+
+
+// var equipment = document.querySelectorAll("ol.equipment > li")
+
+// console.log(equipment[1])
+
+// for (i=0; i < equipment.length; i++){
+//     equipment[i].textContent = "**SECRET**"
+// }
+
+// equipment[3].textContent += " Hang loose"
+
+var text = document.getElementsByTagName("h2").item(0).textContent
+// console.log(text)
+// document.write(text)
+
+var par = document.getElementsByTagName("p").item(0).textContent = text
+
+// document.write(par)
+// .innerHTML = "Here"
